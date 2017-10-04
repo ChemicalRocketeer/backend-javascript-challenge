3bba1f3894c3dc7c15626e9364883bf51ea9c0a7
David Aaron Suddjian <aasuddjian@gmail.com>
2017-10-04 00:44:05 -0700

    dependency injection fix

--- api/images.js
+++ api/images.js
@@ -8,6 +8,13 @@ const flatmap = (arr, lambda) => [].concat(...arr.map(lambda))
 module.exports = ({ flickr }) => {
   const router = express.Router()
 
+  const getSize = async (photo) => {
+    const response = await flickr.photos.getSizesAsync({
+      photo_id: photo.id
+    })
+    return response
+  }
+
   router.get('/', handler(async (req, res, next) => {
     const text = req.query.query
     const response = await flickr.photos.searchAsync({
@@ -15,14 +22,7 @@ module.exports = ({ flickr }) => {
       per_page: 10
     })
     // an array of all the fetches we have to make for photo sizes
-    const fetches = flatmap(response.photos.photo, async (photo) => {
-      return flickr.photos.getSizesAsync({
-        photo_id: photo.id
-      })
-        .then(size => {
-
-        })
-    })
+    const fetches = flatmap(response.photos.photo, getSize)
     await Promise.all(fetches)
     res.json(response.photos.photo)
   }))
--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 const { getFlickr } = require('./flickr-helper')
 const imageApi = require('./api/images')
 const express = require('express')
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-(async () => {
+(async function () {
   const app = express()
 
   const flickr = await getFlickr()
 
-  app.use('/images', imageApi(flickr))
+  app.use('/images', imageApi({ flickr }))
 
   app.listen(PORT)
-})()
+}())

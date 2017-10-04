3c609366e284a0f4cd2b298be056317dd494bb76
David Aaron Suddjian <aasuddjian@gmail.com>
2017-10-04 01:22:16 -0700

    adding server logs

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const { getFlickr } = require('./flickr-helper')
 const imageApi = require('./api/images')
 const express = require('express')
+const morgan = require('morgan')
 const PORT = process.env.PORT || 8080;
 
 (async function () {
   const app = express()
+  app.use(morgan('tiny'))
 
   const flickr = await getFlickr()
 
